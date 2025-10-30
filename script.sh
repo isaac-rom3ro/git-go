e9b4369b3b48aceeffb8cbf4a7225091614ca166
isaac-rom3ro <is4ac.romero@outlook.com>
2025-10-30 10:54:58 -0300

    wip/ver: username, email, ssh

--- script.sh
+++ script.sh
@@ -18,6 +18,26 @@ isGitInstalled () {
   fi
 }
 
+isNameSet() {
+  name=$(git config --global user.name)
+
+  if [[ -z "$name" ]]; then
+    echo false
+  else 
+    echo true
+  fi  
+}
+
+isEmailSet() {
+  email=$(git config --global user.email)
+
+  if [[ -z "$email" ]]; then
+    echo false
+  else 
+    echo true
+  fi 
+}
+
 setName() {
   echo -n "username: "
   read username
@@ -32,9 +52,48 @@ setEmail() {
   git config --global user.email "$email"
 }
 
+sshExists() {
+  sshPublicKey="$HOME/.ssh/id_ed25519.pub"
+
+  if [ -e "$sshPublicKey" ]; then
+    echo true
+  else 
+    echo $(sshGenerate)
+  fi
+}
+
+sshGenerate() {
+  email=$(git config --global user.email)
+  
+  ssh-keygen -t ed25519 -C "$email"
+
+  if [$? -eq 0]; then
+    echo true
+  else 
+    echo false
+  fi
+}
+
 gitInstalled=$(isGitInstalled)
+name=$(isNameSet)
+email=$(isEmailSet)
+ssh=$(sshExists)
 
 if [ gitInstalled ]; then
-  setName  
-  setEmail
-fi
+  if (( name != true)); then
+    setName
+  fi
+
+  if (( email != true)); then
+    setEmail
+  fi
+ 
+  if (( ssh != true )); then
+    echo "Error while trying to generate the ssh"
+    exit 1
+  fi
+
+
+fi 
+
+exit 0
